389b828d52bb0f2b0f68c7f34a01b805b56cabbb
taabelu0 <luca.bertonazzi@students.fhnw.ch>
2025-01-25 15:51:30 +0100

    Initial Commit

---
+++ Project_Two/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project_Two
+//
+//  Created by Luca Bertonazzi on 25.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_Two/Project_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_TwoApp.swift
+//  Project_Two
+//
+//  Created by Luca Bertonazzi on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Project_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
